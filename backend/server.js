35d066ee550eb3bfc550633d5328796fb3ac0fbb
Harikrishnan Kavungal <harikrishnankavungal1993@gmail.com>
2023-12-21 02:28:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,49 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
 dotenv.config();
 const port = process.env.PORT || 5000;
-import userRoutes from './routes/userRoutes.js';
-import bookingRoutes from './routes/bookingRoutes.js';
-import adminRoutes from './routes/adminRoutes.js';
-import slotsRoutes from './routes/slotsRoutes.js';
-import { errorHandler, notFound } from './middleware/errorMiddleware.js';
-import connectDB from './config/db.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-
-
+import userRoutes from "./routes/userRoutes.js";
+import bookingRoutes from "./routes/bookingRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import slotsRoutes from "./routes/slotsRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  }));
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//   })
+// );
 
-app.use('/api/users', userRoutes);
-app.use('/api/bookings', bookingRoutes);
-app.use('/api/slots', slotsRoutes);
-app.use('/api/admin', adminRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/bookings", bookingRoutes);
+app.use("/api/slots", slotsRoutes);
+app.use("/api/admin", adminRoutes);
 
-app.get('/', (req,res) => res.send('Server is ready'));
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Server is ready"));
+}
 
 app.use(notFound);
 app.use(errorHandler);
 
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-
-
-
