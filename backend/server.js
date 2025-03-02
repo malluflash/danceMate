2eb59026c783dd5da53cb1c9439cceec89576873
Harikrishnan K <malluflash@Harikrishnans-Mac-mini.local>
2025-03-02 10:14:12 +0530

    UI changes

--- backend/server.js
+++ backend/server.js
@@ -20,12 +20,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//   })
+// );
 
 app.use("/api/users", userRoutes);
 app.use("/api/bookings", bookingRoutes);
